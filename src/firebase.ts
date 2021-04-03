5c48ee43298ee398f3651f0920e85085155e0380
Who23 <contactwho23@gmail.com>
2021-04-03 18:39:17 -0400

    basic gapi auth

---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/auth'
+
+const firebaseConfig: { [key: string]: string } = {
+    apiKey: "AIzaSyBnQXDP5hR8tbxyFKCP_cUnzN4wKGhFfI8",
+    authDomain: "curo-805dd.firebaseapp.com",
+    projectId: "curo-805dd",
+    storageBucket: "curo-805dd.appspot.com",
+    messagingSenderId: "869858699920",
+    appId: "1:869858699920:web:96651160fcea1e1f1c8615"
+}
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+
+export {
+  auth
+}
